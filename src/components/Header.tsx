de9af62c969e929454e8fa9ea02be6b984fd5aac
Gustavo-Jezini <Gustavojezini@gmail.com>
2023-09-28 12:40:28 -0300

    add: component NewTransactionModal

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,21 +1,5 @@
-import { ArrowCircleDown, ArrowCircleUp, X } from 'phosphor-react'
 import logoImage from '../assets/logo.svg'
-import * as Dialog from '@radix-ui/react-dialog'
-import * as RadioGroup from '@radix-ui/react-radio-group'
-import { tv } from 'tailwind-variants'
-
-const button = tv({
-  base: 'group bg-gray-700 flex p-4 items-center justify-center gap-2 rounded-md cursor-pointer border-0 <text-gray-3></text-gray-3>00',
-
-  variants: {
-    variant: {
-      income:
-        'hover:bg-green-300 hover:text-white data-[state=checked]:bg-green-300 data-[state=checked]:text-white',
-      outcome:
-        'hover:bg-red-600 hover:text-white data-[state=checked]:bg-red-600',
-    },
-  },
-})
+import { NewTransactionModal } from './NewTransactionModal'
 
 export function Header() {
   return (
@@ -23,79 +7,7 @@ export function Header() {
       <div className="mx-auto flex w-full max-w-[1120px] items-center justify-between px-6">
         <img src={logoImage} alt="" />
 
-        <Dialog.Root>
-          <Dialog.Trigger asChild>
-            <button className="h-12 cursor-pointer rounded-md  border-none bg-green-500 px-5 text-white hover:bg-green-700 hover:transition">
-              Nova transação
-            </button>
-          </Dialog.Trigger>
-
-          <Dialog.Portal>
-            <Dialog.Overlay className="fixed inset-0 h-screen w-screen bg-gray-700/75" />
-
-            <Dialog.Content className="fixed left-1/2 top-1/2 min-w-[32rem] -translate-x-1/2 -translate-y-1/2 rounded-md bg-gray-800 px-12 py-10">
-              <Dialog.Title>Nova transação</Dialog.Title>
-
-              <Dialog.Close
-                className="absolute right-6 top-6 cursor-pointer border-0 bg-transparent text-[0px] text-gray-500"
-                asChild
-              >
-                <X size={24} />
-              </Dialog.Close>
-
-              <form action="" className="mt-8 flex flex-col gap-4">
-                <input
-                  className="rounded-md border-0 bg-gray-900 p-4 text-gray-300 placeholder:text-gray-500 focus:border focus:border-green-600"
-                  type="text"
-                  placeholder="Descrição"
-                  required
-                />
-                <input
-                  className="rounded-md border-0 bg-gray-900 p-4 text-gray-300 placeholder:text-gray-500 focus:border focus:border-green-600"
-                  type="number"
-                  placeholder="Preço"
-                  required
-                />
-                <input
-                  className="rounded-md border-0 bg-gray-900 p-4 text-gray-300 placeholder:text-gray-500 focus:border focus:border-green-600"
-                  type="text"
-                  placeholder="Categoria"
-                  required
-                />
-
-                <RadioGroup.Root className="mt-2 grid grid-cols-2 gap-4">
-                  <RadioGroup.Item
-                    value="income"
-                    className={button({ variant: 'income' })}
-                  >
-                    <ArrowCircleUp
-                      size={24}
-                      className="text-green-600 group-hover:text-white group-data-[state=checked]:text-white"
-                    />
-                    Entrada
-                  </RadioGroup.Item>
-                  <RadioGroup.Item
-                    value="outcome"
-                    className={button({ variant: 'outcome' })}
-                  >
-                    <ArrowCircleDown
-                      size={24}
-                      className="text-red-600 group-hover:text-white group-data-[state=checked]:text-white"
-                    />
-                    Saida
-                  </RadioGroup.Item>
-                </RadioGroup.Root>
-
-                <button
-                  className="mt-6 h-14 cursor-pointer rounded-md border-0 bg-green-500 px-5 font-bold text-white hover:bg-green-700 hover:transition"
-                  type="submit"
-                >
-                  Cadastraar
-                </button>
-              </form>
-            </Dialog.Content>
-          </Dialog.Portal>
-        </Dialog.Root>
+        <NewTransactionModal />
       </div>
     </header>
   )
--- src/components/SearchForm.tsx
+++ src/components/SearchForm.tsx
@@ -1,14 +1,45 @@
 import { MagnifyingGlass } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import * as z from 'zod'
+import { zodResolver } from '@hookform/resolvers/zod'
+
+const searchFormSchema = z.object({
+  query: z.string(),
+})
+
+type SearchFormInputs = z.infer<typeof searchFormSchema>
 
 export function SearchForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { isSubmitting },
+  } = useForm<SearchFormInputs>({
+    resolver: zodResolver(searchFormSchema),
+  })
+
+  async function handleSearchTransaction(data: SearchFormInputs) {
+    await new Promise((resolve) => setTimeout(resolve, 2000))
+
+    console.log(data)
+  }
+
   return (
-    <form className="mx-auto mt-10 flex w-full max-w-[1120px] gap-4 px-6">
+    <form
+      onSubmit={handleSubmit(handleSearchTransaction)}
+      className="mx-auto mt-10 flex w-full max-w-[1120px] gap-4 px-6"
+    >
       <input
         placeholder="Báh guri"
         type="text"
         className="flex-1 rounded-md border-0 bg-gray-900 p-4 text-gray-300 placeholder:text-gray-500 focus:border focus:border-green-500"
+        {...register('query')}
       />
-      <button className="flex items-center gap-3 rounded-md border border-green-500 bg-transparent p-4 font-bold text-green-300 hover:border-green-500 hover:bg-green-500 hover:text-white hover:transition">
+      <button
+        disabled={isSubmitting}
+        type="submit"
+        className="flex items-center gap-3 rounded-md border border-green-500 bg-transparent p-4 font-bold text-green-300 hover:border-green-500 hover:bg-green-500 hover:text-white hover:transition disabled:cursor-not-allowed disabled:opacity-60"
+      >
         <MagnifyingGlass size={24} />
         Buttao
       </button>
